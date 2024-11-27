da733084dbcc721d987de4ee76203b98920d20ae
Robby Nugroho Setiawan <47962938+Robbyns99@users.noreply.github.com>
2024-11-27 18:05:23 +0800

    test js

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -2,12 +2,6 @@ const Layarpopup = document.querySelector(".popup");
 const Kontenpopup = document.querySelector(".popup_content");
 const Keluarpopup = document.querySelector(".popup_close");
 
-window.addEventListener("load", () => {
-setTimeout(() => {
-    Layarpopup.classList.add("active");
-    }, 2000);
-});
-
 Keluarpopup.addEventListener("click", () => {
     Layarpopup.classList.remove("active");
 });
